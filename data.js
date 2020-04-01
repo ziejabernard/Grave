69260ec04cf07c7e7ea4a8fb68a1efd03ab80445
Bernard Zieja <ziejabernard@gmail.com>
2020-04-01 16:53:40 +0200

    Add results to tekst area

--- data.js
+++ data.js
@@ -6,12 +6,3 @@ personArray[2] = ["Bogdan", "Mazur", 3, "03.03.2001"];
 personArray[3] = ["Bogdan", "Mazur", 4, "04.04.2001"];
 personArray[4] = ["Bogdan", "Witkowski", 5, "05.03.2001"];
 
-function znajdz(name, surname){
-    name = document.getElementById(`imie`);
-    surname = document.getElementById(`nazwisko`);
-    this.name = name.value;
-    this.surname = surname.value;
-    wynik = personArray.filter(person => person[0] === name.value && person[1] === surname.value);
-    console.log("wynik:");
-    console.log(`Wynik wyszukiawnia ${name.value} ${surname.value} to ${wynik}`);
-}
--- wyszukiwarka.js
+++ wyszukiwarka.js
@@ -1,2 +1,17 @@
 let wynik = new Array;
 
+function znajdz(name, surname){
+    name = document.getElementById(`imie`);
+    surname = document.getElementById(`nazwisko`);
+    this.name = name.value;
+    this.surname = surname.value;
+    wynik = personArray.filter(person => person[0] === name.value && person[1] === surname.value);
+    console.log("wynik:");
+    if(wynik.length !== 0){
+        console.log(`Wynik wyszukiawnia ${name.value} ${surname.value} to ${wynik}`);
+        document.getElementById("wyniki").innerHTML = `Wynik wyszukiawnia ${name.value} ${surname.value} to ${wynik}`;
+    } else {
+        console.log("Niczego nie wyszukano.");
+        document.getElementById("wyniki").innerHTML = `Niczego nie wyszukano.`;
+    }
+}
